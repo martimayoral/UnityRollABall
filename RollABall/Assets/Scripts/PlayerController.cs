c28d3ea7c2d603e368ba67ed681c3a6b01bccef4
martimayoral <76410086+martimayoral@users.noreply.github.com>
2021-04-24 21:12:29 +0200

    Added levels and personalitzations with rotation and stuff

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public GameObject[] lvlWalls;
 
+    private int lvl;
+    private int[] maxBalls = { 10, 15, 20, 25};
     private Rigidbody rb;
     private int count;
     private float mX;
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         count = 0;
         rb = GetComponent<Rigidbody>();
+        lvl = 1;
 
         SetCountText();
         winTextObject.SetActive(false);
@@ -35,16 +39,26 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if(count >= 12)
-            winTextObject.SetActive(true);
+        if (count >= maxBalls[lvl - 1])
+        {
+            if (lvl == 4)
+                winTextObject.SetActive(true);
+            else
+            {
+                lvlWalls[lvl - 1].SetActive(false);
+                count = 0;
+                lvl++;
+            }
+            
+        }
+        countText.text = "Level " + lvl + " - " + count.ToString() + "/" + maxBalls[lvl - 1];
     }
 
     void FixedUpdate()
     {
         Vector3 mv = new Vector3(mX, 0.0f, mY);
 
-        rb.AddForce(mv * speed);
+        rb.AddForce(mv * (speed + lvl));
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ RollABall/Assets/Scripts/WallRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRotator : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 5, 0) * speed * Time.deltaTime);
+    }
+}
