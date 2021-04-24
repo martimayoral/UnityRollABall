aeebf1511fb1a9ff400ccd2d815d6ca3c090a88c
martimayoral <76410086+martimayoral@users.noreply.github.com>
2021-04-24 13:41:45 +0200

    2 steps

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private float mX;
+    private float mY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue mVal)
+    {
+        Vector2 mVec = mVal.Get<Vector2>();
+
+        mX = mVec.x;
+        mY = mVec.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 mv = new Vector3(mX, 0.0f, mY);
+
+        rb.AddForce(mv * speed);
+    }
+}
