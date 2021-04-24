c8fc4068488c05927266073d0477eef778f02df1
martimayoral <76410086+martimayoral@users.noreply.github.com>
2021-04-24 16:08:41 +0200

    game finished with build

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float mX;
     private float mY;
 
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue mVal)
@@ -25,10 +33,27 @@ public class PlayerController : MonoBehaviour
         mY = mVec.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+            winTextObject.SetActive(true);
+    }
+
     void FixedUpdate()
     {
         Vector3 mv = new Vector3(mX, 0.0f, mY);
 
         rb.AddForce(mv * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 }
